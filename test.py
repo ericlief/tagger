6a70538388d7d5361964bca21618d512f4cac707
Eric Lief <liefe@bossa>
2017-09-12 20:53:04 +0200

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+    Project: Code
+    File: test
+    Version: 
+    Created by liefe on 12.9.17.
+
+"""
+
+with open('test.txt', 'a') as f:
+    f.write('test')
+
